105ab5dd9ac9dbbbdb7a26c00c24e17f97536e5c
vineetkrsingh769 <vineetkr769@gmail.com>
2026-04-24 16:33:10 +0530

    3 commit

--- model/code/__torch__/torch/nn/modules/dropout.py
+++
@@ -1,9 +0,0 @@
-class Dropout(Module):
-  __parameters__ = []
-  __buffers__ = []
-  training : bool
-  _is_full_backward_hook : Optional[bool]
-  def forward(self: __torch__.torch.nn.modules.dropout.Dropout,
-    argument_1: Tensor) -> Tensor:
-    input = torch.dropout(argument_1, 0.5, False)
-    return input
--- model/code/__torch__/torch/nn/modules/dropout/___torch_mangle_64.py
+++
@@ -1,9 +0,0 @@
-class Dropout(Module):
-  __parameters__ = []
-  __buffers__ = []
-  training : bool
-  _is_full_backward_hook : Optional[bool]
-  def forward(self: __torch__.torch.nn.modules.dropout.___torch_mangle_64.Dropout,
-    argument_1: Tensor) -> Tensor:
-    input = torch.dropout(argument_1, 0.25, False)
-    return input
